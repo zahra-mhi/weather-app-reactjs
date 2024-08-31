91103db95719c936ed8792d23cfc6eaf10b0ec41
Zahra Mehri <zahra.m@4D.local>
2024-08-31 15:38:15 +0330

    some style

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   }
 
   return (
-    <div className="container mt-4">
+    <div className="container mt-4 p-5 bg-light border rounded-5">
       <h2 className="text-center mb-3">Welcome to My weather App</h2>
       <label className="mb-2">please select city :</label>
       <select className="form-select" onChange={handleChange}>
